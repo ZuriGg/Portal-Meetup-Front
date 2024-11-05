9baca561bd45c1a44a2707e3c1d1b8ac1cbee70d
zuri <zurimanes@gmail.com>
2024-11-05 07:45:14 +0100

    Corrijo problemas en las rutas

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,37 +1,52 @@
 import React from "react";
-import { Navigate, Route, Route } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 import Home from "./Home/Home.jsx";
-import ValidateUser from "./Validate/ValidateUser.jsx";
-import Register from "./Register/Register.jsx";
-import CreateMeetup from "./Create/CreateMeetup.jsx";
-import RecoveryPass from "./Recovery/RecoveryPass.jsx";
-import Login from "./Login/Login.jsx";
-import EditMeetup from "./Edit/EditMeetup.jsx";
 
-const Routes = () => (
+// Import de rutas de usuarios
+import ValidateUser from "./Users/Validate/ValidateUser.jsx";
+import Register from "./Users/Register/Register.jsx";
+import Login from "./Users/Login/Login.jsx";
+
+// Import de rutas de gestión de contraseñas
+import RecoveryPass from "./Users/Password/Recovery/RecoveryPass.jsx";
+
+// Import de rutas de meetups
+import CreateMeetup from "./Meetups/Create/CreateMeetup.jsx";
+import EditMeetup from "./Meetups/Edit/EditMeetup.jsx";
+
+const AppRoutes = () => (
     <>
-        <Route index element={<Home />} />
-        {/* Aqui iria el Home según sea usuario anonimo o registrado */}
-        <Route path="user">
-            <Route path="register" element={<Register />} />
-            {/* Aqui iria la página de registro de usuario */}
-            <Route path="Validate" element={<ValidateUser />} />
-            {/* Aquí iria la página donde el usuario es informado de que está validado */}
-            <Route path="Login" element={<Login />} />
-            {/* Aquí iria la página para iniciar sesión */}
-            <Route path="password">
-                <Route path="recover" element={<RecoveryPass />} />
-                {/* Aquí iria la página para solicitar la recuperación de la contraseña */}
+        <Routes>
+            {/* Aqui iria el Home según sea usuario anónimo o registrado */}
+            <Route path="/" element={<Home />} />
+
+            {/* Rutas de usuario */}
+            <Route path="user">
+                {/* Aqui iria la página de registro de usuario */}
+                <Route path="register" element={<Register />} />
+                {/* Aquí iria la página donde el usuario es informado de que está validado */}
+                <Route path="Validate" element={<ValidateUser />} />
+                {/* Aquí iria la página para iniciar sesión */}
+                <Route path="Login" element={<Login />} />
+                <Route path="password">
+                    {/* Aquí iria la página para solicitar la recuperación de la contraseña */}
+                    <Route path="recover" element={<RecoveryPass />} />
+                </Route>
             </Route>
-        </Route>
-        <Route path="meetup">
-            <Route path="create" element={<CreateMeetup />} />
-            {/* Aquí iria la página para crear un meetup */}
-            <Route path="edit" element={<EditMeetup />} />
-        </Route>
-        <Route path="*" element={<Navigate to="/" replace />} />
+
+            {/* Rutas de meetups */}
+            <Route path="meetup">
+                {/* Aquí iria la página para crear un meetup */}
+                <Route path="create" element={<CreateMeetup />} />
+                {/* Aquí iria la página para editar un meetup */}
+                <Route path="edit" element={<EditMeetup />} />
+            </Route>
+
+            {/* Ruta para redirigir cualquier ruta no válida */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
     </>
 );
 
-export default Routes;
+export default AppRoutes;
