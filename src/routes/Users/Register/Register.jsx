a09a908f43a38d6e60598e5a9476d35552f1e2b3
Paula Fernández <better_days1996@hotmail.com>
2024-11-05 11:49:30 +0100

    importando el contexto de usuario en cada ruta en la que se usará

--- src/routes/Users/Register/Register.jsx
+++ src/routes/Users/Register/Register.jsx
@@ -1,7 +1,9 @@
 // Página para el registro de usuario
+import { useUser } from "../../../UserContext.jsx";
 import "./Register.css";
 
 function Register() {
+  const [user, setUser] = useUser(); //importamos el contexto de usuario
   return <div>Register</div>;
 }
 
