aaf0d34b4a04b56b40ac5d8ced0d9d4d4a186a3e
zuri <zurimanes@gmail.com>
2024-11-06 06:24:08 +0100

    Actualizo useFetch para que admita solicitudes post

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -4,24 +4,24 @@ import { createContext, useContext, useState } from "react";
 export const UserContext = createContext();
 export const useUser = () => useContext(UserContext);
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(
-    JSON.parse(localStorage.getItem("session")) || null
-  );
+    const [user, setUser] = useState(
+        JSON.parse(localStorage.getItem("session")) || null
+    );
 
-  const enhancedSetUser = (better) => {
-    setUser(better);
-    localStorage.setItem("session", JSON.stringify(better));
-  };
+    const enhancedSetUser = (better) => {
+        setUser(better);
+        localStorage.setItem("session", JSON.stringify(better));
+    };
 
-  return (
-    <UserContext.Provider value={[user, enhancedSetUser]}>
-      {children}
-    </UserContext.Provider>
-  );
+    return (
+        <UserContext.Provider value={[user, enhancedSetUser]}>
+            {children}
+        </UserContext.Provider>
+    );
 };
 
 // para llamar usar el contexto, solo hay que llamarlo en cada componente: const [user, setUser] = useUser();
 
 UserProvider.propTypes = {
-  children: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired,
 };
