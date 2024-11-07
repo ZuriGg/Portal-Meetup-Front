a0e10aba8bd24079b501e56578bdb2947a495750
zuri <zurimanes@gmail.com>
2024-11-07 21:22:02 +0100

    Actualizo corrección de merge de ramas

--- src/Pages/Users/Validate/Validate.jsx
+++ src/Pages/Users/Validate/Validate.jsx
@@ -1,16 +1,18 @@
-import { Link } from "react-router-dom";
-import "./Validate.css";
+import { useNavigate } from "react-router-dom";
 
 function Validate() {
+  const navigate = useNavigate();
+
+  const handleclick = () => {
+    navigate("/user/login");
+  };
   return (
     <>
       <h1>pagina de validacion </h1>
       <p>Usuario verificado correctamente</p>
-      <Link to="/loging">
-        <button className="validateButton" type="button">
-          Iniciar sesión
-        </button>
-      </Link>
+      <button className="validateButton" type="button" onClick={handleclick}>
+        Iniciar sesión
+      </button>
     </>
   );
 }
