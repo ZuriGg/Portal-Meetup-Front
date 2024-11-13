5681701ad040945d0d4f79f4c94bd26849754c8c
Paula Fernández <better_days1996@hotmail.com>
2024-11-13 18:45:07 +0100

    implementando footer sin css

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,111 @@
+import { Link } from 'react-router-dom';
 // Footer en común para todo el sitio web
 import './Footer.css';
 
 function Footer() {
     return (
-        <>
-            <p>Tonto el que lo lea</p>
-        </>
+        <footer>
+            <nav id="container-footer">
+                <h3>
+                    <Link to="/users">Tu cuenta</Link>{' '}
+                    {/* Link para rutas internas dentro de nuestra aplicación */}
+                </h3>
+                <h3>
+                    <Link to="/meetups">Meet ups por categoría</Link>{' '}
+                </h3>
+                <div>
+                    <Link
+                        to={`/categories/${encodeURIComponent(
+                            'Viajes y aire libre'
+                        )}`}
+                    >
+                        Viajes y aire libre
+                    </Link>
+                    <Link
+                        to={`/categories/${encodeURIComponent(
+                            'Actividades sociales'
+                        )}`}
+                    >
+                        Actividades sociales
+                    </Link>
+                    <Link
+                        to={`/categories/${encodeURIComponent(
+                            'Aficiones y pasiones'
+                        )}`}
+                    >
+                        Aficiones y pasiones
+                    </Link>
+                    <Link
+                        to={`/categories/${encodeURIComponent(
+                            'Deportes y fitness'
+                        )}`}
+                    >
+                        Deportes y fitness
+                    </Link>
+                    <Link
+                        to={`"/categories/${encodeURIComponent(
+                            'Salud y bienestar'
+                        )}`}
+                    >
+                        Salud y bienestar
+                    </Link>
+                    <Link
+                        to={`/categories/${encodeURIComponent('Tecnología')}`}
+                    >
+                        Tecnología
+                    </Link>
+                    <Link
+                        to={`/categories/${encodeURIComponent(
+                            'Arte y cultura'
+                        )}`}
+                    >
+                        Arte y cultura
+                    </Link>
+                    <Link to={`/categories/${encodeURIComponent('Juegos')}`}>
+                        Juegos
+                    </Link>
+                </div>
+
+                <h3>Conócenos</h3>
+                <p>
+                    <a /* a para rutas externas a direcciones fuera de nuestra API */
+                        href={
+                            'https://www.linkedin.com/in/sergio-manzano-esclapez/'
+                        }
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Sergio
+                    </a>
+                    <a
+                        href={
+                            'https://www.linkedin.com/in/jonathanmendezromanelli/'
+                        }
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Jonathan
+                    </a>
+                    <a
+                        href={
+                            'https://www.linkedin.com/in/paulafernandezrivera/'
+                        }
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Paula
+                    </a>
+                    <a
+                        href={'https://www.linkedin.com/in/diegoivan/'}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Diego
+                    </a>
+                </p>
+            </nav>
+            <p>©2024 Meetups</p>
+        </footer>
     );
 }
 
