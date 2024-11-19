f65aab2407944bd8c8d4be761513ddc04cda3dd1
Paula Fernández <better_days1996@hotmail.com>
2024-11-19 15:41:08 +0100

    implementando feedback "En ChangePass no se usa const [user] = useUser();"

--- src/Pages/Users/Password/changePass/ChangePass.jsx
+++ src/Pages/Users/Password/changePass/ChangePass.jsx
@@ -2,18 +2,13 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import './ChangePass.css';
-import { useUser } from '../../../../UserContext.jsx';
 
 export default function ChangePass() {
-    // const [email, setEmail] = useState(''); --> NO HACE FALTA PORQUE LO IMPORTAMOS DESDE EL USERCONTEXT
     const [recoverPassCode, setRecoverPassCode] = useState('');
     const [newPass, setNewPass] = useState('');
     const [error, setError] = useState(null);
     const [success, setSuccess] = useState(false);
     const navigate = useNavigate();
-
-    // Usamos el contexto de usuario
-    const [user] = useUser();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -25,7 +20,6 @@ export default function ChangePass() {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                email: user.email, //usamos el email del usuario logueado
                 recoverPassCode,
                 newPass,
             }),
@@ -45,21 +39,9 @@ export default function ChangePass() {
     return (
         <>
             <h1>Página de cambio de contraseña</h1>
-            <p>
-                Inserte {/* su correo electrónico,*/} su código de contraseña y
-                su nueva contraseña
-            </p>
+            <p>Inserte su código de contraseña y su nueva contraseña</p>
             <div id="recover" className="page">
                 <form onSubmit={handleSubmit}>
-                    {/* <label>
-                        <span>Email: </span>
-                        <input
-                            type="text"
-                            name="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </label>  SE SUPONE QUE CUANDO CAMBIAS TU CONTRASEÑA YA ESTÁS LOGUEADO, ASÍ QUE NO HARÍA FALTA RELLENAR ESTE CAMPO*/}
                     <label>
                         <span>Código de cambio de contraseña:</span>
                         <input
@@ -77,6 +59,7 @@ export default function ChangePass() {
                             value={newPass}
                             onChange={(e) => setNewPass(e.target.value)}
                         />
+                        {/* MODIFICAR PARA QUE APAREZCAN DOS CAMPOS DE NUEVA CONTRASEÑA Y QUE SE COMPARE DESDE EL MISMO FRONT. LLEVARSE DE REGISTER.JSX */}
                         <button>✔</button>
                         {success && <p>Cambio de contraseña realizada</p>}
                         {error && <p>{error}</p>}
