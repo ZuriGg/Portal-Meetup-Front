460a1bc59a7ebb082817808c3099b9cbaadc17a6
dicalmaestra <dicalmaestra@gmail.com>
2024-11-21 21:07:07 +0100

    notFound 3.0

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -8,7 +8,7 @@ const NotFound = () => {
                 <h1>🤭 Página No Encontrada, creo que es error 400 noséqué😅</h1> 
                 <p>Lo sentimos, pero la página que buscas no existe.</p> 
                 <p>No nos vamos a engañar....has escrito mal la dirección 🤷‍♂️</p> 
-                <Link to="/" >Vuelve a intentarlo ooootra vez</Link>
+                <Link to="/" className='comeBack'>Vuelve a intentarlo ooootra vez</Link>
             </div>
         </div>
 
