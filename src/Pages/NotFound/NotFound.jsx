88642c1be6092ec029c3f06f0f801b065a1d7c69
dicalmaestra <dicalmaestra@gmail.com>
2024-11-21 21:26:24 +0100

    notFound with no footerCSS

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './NotFound.css';
+
 
 const NotFound = () => {
     return (
-        <div className="notFoundBootle">
             <div className='notFoundLiquid'>
                 <h1>🤭 Página No Encontrada, creo que es error 400 noséqué😅</h1> 
                 <p>Lo sentimos, pero la página que buscas no existe.</p> 
                 <p>No nos vamos a engañar....has escrito mal la dirección 🤷‍♂️</p> 
-                <Link to="/" className='comeBack'>Vuelve a intentarlo ooootra vez</Link>
+                <Link to="/" className='comeBack'>Vuelve a intentarlo ooootra vez 🩷</Link>
             </div>
-        </div>
+        
 
     )
 }
