4eed84ee84a90d507cb67d07aea8c45ba4273349
zuri <zurimanes@gmail.com>
2024-12-04 01:55:52 +0100

    Revert "A medio implementar votos medios en meetupCard en home"
    This reverts commit 558c4340c645d2da1c6bbbeef738f046e5dd994f.

--- src/Components/MeetupCard/MeetupCard.jsx
+++ src/Components/MeetupCard/MeetupCard.jsx
@@ -8,8 +8,6 @@ function MeetupCard({
     startDate,
     hourMeetup,
     aforoMax,
-    averageRating,
-    location,
 }) {
     return (
         <div className="meetupsCard">
@@ -22,8 +20,6 @@ function MeetupCard({
                 {dayOfTheWeek} - {new Date(startDate).toLocaleDateString()} -{' '}
                 {hourMeetup}
             </p>
-            <p>{averageRating}</p>
-            <p>{/* location */}</p>
 
             <p>Aforo máximo: {aforoMax}</p>
         </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -29,31 +29,7 @@ function Home() {
                 );
                 if (!response.ok) throw new Error('Error fetching data');
                 const data = await response.json();
-
-                // Obtén información adicional como valoraciones y ubicaciones
-                const additionalDataPromises = data.data.map(async (meetup) => {
-                    const [votesRes, locationRes] = await Promise.all([
-                        fetch(
-                            `http://localhost:3000/meetups/${meetup.id}/votes`
-                        ),
-                        fetch(
-                            `http://localhost:3000/location/${meetup.locationId}`
-                        ),
-                    ]);
-
-                    const votesData = await votesRes.json();
-                    const locationData = await locationRes.json();
-
-                    return {
-                        ...meetup,
-                        averageRating: votesData?.average || 0, // Valoración media
-                        location: locationData.data || {}, // Asigna el objeto de ubicación
-                    };
-                });
-
-                const enrichedData = await Promise.all(additionalDataPromises);
-
-                setResults(enrichedData); // Actualizamos el estado con datos enriquecidos
+                setResults(data.data || []);
                 setLoading(false); // Termina de cargar después de obtener los meetups
             } catch (err) {
                 setError(err.message);
@@ -183,8 +159,6 @@ function Home() {
                                                 .toUpperCase() +
                                             meetup.dayOfTheWeek.slice(1)
                                         }
-                                        averageRating={meetup.averageRating}
-                                        location={meetup.location} // Objeto único correctamente asignado
                                     />
                                 </Link>
                             </li>
